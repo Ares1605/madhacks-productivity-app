382111d68d177caf30fecfca497d88ed103f20e0
David Bodansky <dboda2@uic.edu>
2024-11-09 23:02:27 -0600

    Second version

---
+++ backend/dummy_app.py
@@ -0,0 +1,72 @@
+import json
+from os import environ as env
+from urllib.parse import quote_plus, urlencode
+
+from authlib.integrations.flask_client import OAuth
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, redirect, render_template, session, url_for
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
+app = Flask(__name__)
+app.secret_key = env.get("SECRET_KEY")
+
+
+oauth = OAuth(app)
+
+oauth.register(
+    "auth0",
+    client_id=env.get("AUTH0_CLIENT_ID"),
+    client_secret=env.get("AUTH0_CLIENT_SECRET"),
+    client_kwargs={
+        "scope": "openid profile email",
+    },
+    server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration',
+)
+
+
+# Controllers API
+@app.route("/ping")
+def ping():
+    return "ping"
+@app.route("/items/<user_id>")
+def get_user_items(user_id):
+    return [{"section": "Productivity", "date_from": "09-11-2024 05:00:00", "duration": "60"}, {"section": "Entertainment", "date_from": "09-11-2024 07:30:25", "duration": "25"}, {"section": "Productivity", "date_from": "09-11-2024 15:08:30", "duration": "95"}]
+@app.route("/items/<user_id>/set")
+def set_user_items(user_id):
+    return {"success": True, "message": "Successfully added item"}
+
+@app.route("/callback", methods=["GET", "POST"])
+def callback():
+    token = oauth.auth0.authorize_access_token()
+    session["user"] = token
+    return redirect("/")
+
+@app.route("/login")
+def login():
+    return oauth.auth0.authorize_redirect(
+        redirect_uri=url_for("callback", _external=True)
+    )
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(
+        "https://"
+        + env.get("AUTH0_DOMAIN")
+        + "/v2/logout?"
+        + urlencode(
+            {
+                "returnTo": url_for("home", _external=True),
+                "client_id": env.get("AUTH0_CLIENT_ID"),
+            },
+            quote_via=quote_plus,
+        )
+    )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=env.get("PORT", 5000))
+
